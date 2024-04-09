7b7760df607307147ae61169584a3f0589b23193
Anny Dias <127064019+anny-dias@users.noreply.github.com>
2024-04-09 11:07:25 -0300

    Add files via upload

---
+++ 090424/src/main/java/br/com/fiap/ComparaString.java
@@ -0,0 +1,24 @@
+package br.com.fiap;
+ 
+import javax.swing.JOptionPane;
+ 
+public class ComparaString {
+ 
+	public static void main(String[] args) {
+		String senha;
+		senha = JOptionPane.showInputDialog("Digite sua senha");
+		// diferenciando minusculas e maiusculas
+		if (senha.equals("P4ssw0rd")) {
+			JOptionPane.showMessageDialog(null, "Teste 1: Acesso autorizado!");
+		}else {
+			JOptionPane.showMessageDialog(null, "Teste 1: Acesso negado!");
+		}
+		//sem diferenciação de minuscula e maiuscula
+		if (senha.equalsIgnoreCase("P4ssw0rd")) {
+			JOptionPane.showMessageDialog(null, "Teste 2: Acesso autorizado!");
+		}else {
+			JOptionPane.showMessageDialog(null, "Teste 2: Acesso negado!");
+		}
+	}
+ 
+}
